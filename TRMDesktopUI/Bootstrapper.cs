045fc0bd77e727144ef3b6fe07dd7e8baea526e0
Krzysztof Mazanek <kmazanek@gmail.com>
2021-07-23 14:30:00 +0200

    Add simple container DI system to DesktopUI.

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,7 @@
 ﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using TRMDesktopUI.ViewModels;
 
@@ -6,14 +9,46 @@ namespace TRMDesktopUI
 {
 	public class Bootstrapper : BootstrapperBase
 	{
+		private SimpleContainer container = new SimpleContainer();
+
 		public Bootstrapper()
 		{
 			Initialize();
 		}
 
+		protected override void Configure()
+		{
+			container.Instance(container);
+
+			container
+				.Singleton<IWindowManager, WindowManager>()
+				.Singleton<IEventAggregator, EventAggregator>();
+
+			GetType().Assembly.GetTypes()
+				.Where(type => type.IsClass)
+				.Where(type => type.Name.EndsWith("ViewModel"))
+				.ToList().ForEach(viewModelType => 
+					container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+		}
+
 		protected override void OnStartup(object sender, StartupEventArgs e)
 		{
 			DisplayRootViewFor<ShellViewModel>();
+		}
+
+		protected override object GetInstance(Type service, string key)
+		{
+			return container.GetInstance(service, key);
+		}
+
+		protected override IEnumerable<object> GetAllInstances(Type service)
+		{
+			return container.GetAllInstances(service);
+		}
+
+		protected override void BuildUp(object instance)
+		{
+			container.BuildUp(instance);
 		}
 	}
 }
