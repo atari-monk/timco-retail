8b9275170ee6182d070602021c17c6db333f1d64
Krzysztof Mazanek <kmazanek@gmail.com>
2021-07-27 15:26:53 +0200

    Add Sales Page
    Adds sales page xaml and just scafolds it's vm with no logic.

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,111 @@
+﻿using Caliburn.Micro;
+using System.ComponentModel;
+
+namespace TRMDesktopUI.ViewModels
+{
+	public class SalesViewModel : Screen
+	{
+		private BindingList<string> products;
+		private BindingList<string> cart;
+		private string itemQuantity;
+
+		public BindingList<string> Products
+		{
+			get { return products; }
+			set 
+			{ 
+				products = value;
+				NotifyOfPropertyChange(() => Products);
+			}
+		}
+
+		public BindingList<string> Cart
+		{
+			get { return cart; }
+			set 
+			{ 
+				cart = value;
+				NotifyOfPropertyChange(() => Cart);
+			}
+		}
+
+		public string ItemQuantity
+		{
+			get { return itemQuantity; }
+			set 
+			{ 
+				itemQuantity = value;
+				NotifyOfPropertyChange(() => ItemQuantity);
+			}
+		}
+
+		public string SubTotal 
+		{ 
+			get
+			{
+				return "$0.00";
+			}
+		}
+
+		public string Tax
+		{
+			get
+			{
+				return "$0.00";
+			}
+		}
+
+		public string Total
+		{
+			get
+			{
+				return "$0.00";
+			}
+		}
+
+		public bool CanAddToCart
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public void AddToCart()
+		{
+
+		}
+
+		public bool CanRemoveFromCart
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public void RemoveFromCart()
+		{
+
+		}
+
+		public bool CanCheckOut
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public void CheckOut()
+		{
+
+		}
+	}
+}
