8664f4ea20b6bccbbf3e1758c3299bbc283121cf
krzm <kmazanek@gmail.com>
2021-09-20 14:37:28 +0200

    Add transactions.
    Add transactions to sql data access
    and use them for salesData call.

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -53,20 +53,34 @@ namespace TRMDataManager.Library.DataAccess
 
 			sale.Total = sale.SubTotal + sale.Tax;
 
-			//Save the same model
+			//Save the sale model
 
-			var sql = new SqlDataAccess();
-			sql.SaveData("dbo.spSale_Insert", sale, "TRMData");
-
-			//Get the ID from the sale model
-			sale.Id = sql.LoadData<int, dynamic>("spSale_Lookup", new { sale.CashierId, sale.SaleDate }, "TRMData").FirstOrDefault();
-
-			//Finish filling in the sale detail models
-			foreach (var item in details)
+			using (var sql = new SqlDataAccess())
 			{
-				item.SaleId = sale.Id;
-				//Save the same detail models
-				sql.SaveData("dbo.spSaleDetail_Insert", item, "TRMData");
+				try
+				{
+					sql.StartTransaction("TRMData");
+
+					sql.SaveDataInTransaction("dbo.spSale_Insert", sale);
+
+					//Get the ID from the sale model
+					sale.Id = sql.LoadDataInTransaction<int, dynamic>("spSale_Lookup", new { sale.CashierId, sale.SaleDate }).FirstOrDefault();
+
+					//Finish filling in the sale detail models
+					foreach (var item in details)
+					{
+						item.SaleId = sale.Id;
+						//Save the same detail models
+						sql.SaveDataInTransaction("dbo.spSaleDetail_Insert", item);
+					}
+
+					sql.CommitTransaction();
+				}
+				catch
+				{
+					sql.RollbackTransaction();
+					throw;
+				}
 			}
 		}
 	}
