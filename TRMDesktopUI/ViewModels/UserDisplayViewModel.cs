ce26c5cd64296accc497a1b51dc17b3f1e6cc713
krzm <kmazanek@gmail.com>
2022-08-02 20:07:31 +0200

    rm warn

--- TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -8,209 +8,209 @@ using System.Windows;
 using TRMDesktopUI.Library.Api;
 using TRMDesktopUI.Library.Models;
 
-namespace TRMDesktopUI.ViewModels
+namespace TRMDesktopUI.ViewModels;
+
+public class UserDisplayViewModel : Screen
 {
-	public class UserDisplayViewModel : Screen
+	private readonly StatusInfoViewModel status;
+	private readonly IWindowManager windowManager;
+	private readonly IUserEndpoint userEndpoint;
+	private BindingList<UserModel> users;
+	private UserModel selectedUser;
+	private string selectedUserName;
+	private BindingList<string> userRoles = new();
+	private BindingList<string> availableRoles = new();
+	private string selectedUserRole;
+	private string selectedAvailableRole;
+
+	public BindingList<UserModel> Users
 	{
-		private readonly StatusInfoViewModel status;
-		private readonly IWindowManager windowManager;
-		private readonly IUserEndpoint userEndpoint;
-		private BindingList<UserModel> users;
-		private UserModel selectedUser;
-		private string selectedUserName;
-		private BindingList<string> userRoles = new();
-		private BindingList<string> availableRoles = new();
-		private string selectedUserRole;
-		private string selectedAvailableRole;
-
-		public BindingList<UserModel> Users
-		{
-			get {
-				return users;
-			}
-			set {
-				users = value;
-				NotifyOfPropertyChange(() => Users);
-			}
+		get {
+			return users;
 		}
-
-		public UserModel SelectedUser
-		{
-			get {
-				return selectedUser;
-			}
-			set {
-				selectedUser = value;
-				SelectedUserName = value.Email;
-				UserRoles.Clear();
-				UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
-				//todo:pull this out into a method/event
-				LoadRoles();
-				NotifyOfPropertyChange(() => SelectedUser);
-			}
+		set {
+			users = value;
+			NotifyOfPropertyChange(() => Users);
 		}
+	}
 
-		public string SelectedUserRole
-		{
-			get {
-				return selectedUserRole;
-			}
-			set {
-				selectedUserRole = value;
-				NotifyOfPropertyChange(() => SelectedUserRole);
-				NotifyOfPropertyChange(() => CanRemoveSelectedRole);
-			}
+	public UserModel SelectedUser
+	{
+		get {
+			return selectedUser;
 		}
-
-		public string SelectedAvailableRole
-		{
-			get {
-				return selectedAvailableRole;
-			}
-			set {
-				selectedAvailableRole = value;
-				NotifyOfPropertyChange(() => SelectedAvailableRole);
-				NotifyOfPropertyChange(() => CanAddSelectedRole);
-			}
+		set {
+			selectedUser = value;
+			SelectedUserName = value.Email;
+			UserRoles.Clear();
+			UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+			LoadRoles();
+			NotifyOfPropertyChange(() => SelectedUser);
 		}
+	}
 
-		public string SelectedUserName
-		{
-			get {
-				return selectedUserName;
-			}
-			set {
-				selectedUserName = value;
-				NotifyOfPropertyChange(() => SelectedUserName);
-			}
+	public string SelectedUserRole
+	{
+		get {
+			return selectedUserRole;
 		}
-
-		public BindingList<string> UserRoles
-		{
-			get {
-				return userRoles;
-			}
-			set {
-				userRoles = value;
-				NotifyOfPropertyChange(() => UserRoles);
-			}
+		set {
+			selectedUserRole = value;
+			NotifyOfPropertyChange(() => SelectedUserRole);
+			NotifyOfPropertyChange(() => CanRemoveSelectedRole);
 		}
+	}
 
-		public BindingList<string> AvailableRoles
-		{
-			get {
-				return availableRoles;
-			}
-			set {
-				availableRoles = value;
-				NotifyOfPropertyChange(() => AvailableRoles);
-			}
+	public string SelectedAvailableRole
+	{
+		get {
+			return selectedAvailableRole;
 		}
-
-		public bool CanAddSelectedRole
-		{
-			get {
-				if (SelectedUser is null || SelectedAvailableRole is null)
-				{
-					return false;
-				}
-				else
-				{
-					return true;
-				}
-			}
+		set {
+			selectedAvailableRole = value;
+			NotifyOfPropertyChange(() => SelectedAvailableRole);
+			NotifyOfPropertyChange(() => CanAddSelectedRole);
 		}
+	}
 
-		public bool CanRemoveSelectedRole
-		{
-			get {
-				if (SelectedUser is null || SelectedUserRole is null)
-				{
-					return false;
-				}
-				else
-				{
-					return true;
-				}
-			}
+	public string SelectedUserName
+	{
+		get {
+			return selectedUserName;
 		}
-
-		public UserDisplayViewModel(
-			StatusInfoViewModel status
-			, IWindowManager windowManager
-			, IUserEndpoint userEndpoint)
-		{
-			this.status = status;
-			this.windowManager = windowManager;
-			this.userEndpoint = userEndpoint;
+		set {
+			selectedUserName = value;
+			NotifyOfPropertyChange(() => SelectedUserName);
 		}
+	}
 
-		protected async override void OnViewLoaded(object view)
-		{
-			base.OnViewLoaded(view);
-			try
+	public BindingList<string> UserRoles
+	{
+		get {
+			return userRoles;
+		}
+		set {
+			userRoles = value;
+			NotifyOfPropertyChange(() => UserRoles);
+		}
+	}
+
+	public BindingList<string> AvailableRoles
+	{
+		get {
+			return availableRoles;
+		}
+		set {
+			availableRoles = value;
+			NotifyOfPropertyChange(() => AvailableRoles);
+		}
+	}
+
+	public bool CanAddSelectedRole
+	{
+		get {
+			if (SelectedUser is null || SelectedAvailableRole is null)
 			{
-				await LoadUsers();
+				return false;
 			}
-			catch (Exception ex)
+			else
 			{
-				dynamic settings = new ExpandoObject();
-				settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-				settings.ResizeMode = ResizeMode.NoResize;
-				settings.Title = "System Error";
-
-				if (ex.Message == "Unauthorized")
-				{
-					status.UpdateMessage(
-						"Unauthorized Access"
-						, "You dont have permission to interact with the Sales Form.");
-					await windowManager.ShowDialogAsync(status, null, settings);
-				}
-				else
-				{
-					status.UpdateMessage(
-						"FatalException"
-						, ex.Message);
-					await windowManager.ShowDialogAsync(status, null, settings);
-				}
-
-				await TryCloseAsync();
+				return true;
 			}
 		}
+	}
 
-		private async Task LoadUsers()
-		{
-			var userList = await userEndpoint.GetAll();
-			Users = new BindingList<UserModel>(userList);
-		}
-
-		private async Task LoadRoles()
-		{
-			AvailableRoles.Clear();
-			var roles = await userEndpoint.GetAllRoles();
-			foreach (var role in roles)
+	public bool CanRemoveSelectedRole
+	{
+		get {
+			if (SelectedUser is null || SelectedUserRole is null)
 			{
-				if (UserRoles.IndexOf(role.Value) < 0)
-				{
-					AvailableRoles.Add(role.Value);
-				}
+				return false;
+			}
+			else
+			{
+				return true;
 			}
 		}
+	}
 
-		public async void AddSelectedRole()
+	public UserDisplayViewModel(
+		StatusInfoViewModel status
+		, IWindowManager windowManager
+		, IUserEndpoint userEndpoint)
+	{
+		this.status = status;
+		this.windowManager = windowManager;
+		this.userEndpoint = userEndpoint;
+	}
+
+	protected async override void OnViewLoaded(object view)
+	{
+		base.OnViewLoaded(view);
+		try
 		{
-			await userEndpoint.AddUserToRole(SelectedUser.Id, SelectedAvailableRole);
-
-			UserRoles.Add(SelectedAvailableRole);
-			AvailableRoles.Remove(SelectedAvailableRole);
+			await LoadUsers();
 		}
-
-		public async void RemoveSelectedRole()
+		catch (Exception ex)
 		{
-			await userEndpoint.RemoveUserFromRole(SelectedUser.Id, SelectedUserRole);
+			dynamic settings = new ExpandoObject();
+			settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+			settings.ResizeMode = ResizeMode.NoResize;
+			settings.Title = "System Error";
 
-			AvailableRoles.Add(SelectedUserRole);
-			UserRoles.Remove(SelectedUserRole);
+			if (ex.Message == "Unauthorized")
+			{
+				status.UpdateMessage(
+					"Unauthorized Access"
+					, "You dont have permission to interact with the Sales Form.");
+				await windowManager.ShowDialogAsync(status, null, settings);
+			}
+			else
+			{
+				status.UpdateMessage(
+					"FatalException"
+					, ex.Message);
+				await windowManager.ShowDialogAsync(status, null, settings);
+			}
+
+			await TryCloseAsync();
 		}
+	}
+
+	private async Task LoadUsers()
+	{
+		var userList = await userEndpoint.GetAll();
+		Users = new BindingList<UserModel>(userList);
+	}
+
+	private void LoadRoles()
+	{
+		AvailableRoles.Clear();
+        var task = userEndpoint.GetAllRoles();
+        task.RunSynchronously();
+		var roles = task.Result;
+		foreach (var role in roles)
+		{
+			if (UserRoles.IndexOf(role.Value) < 0)
+			{
+				AvailableRoles.Add(role.Value);
+			}
+		}
+	}
+
+	public async void AddSelectedRole()
+	{
+		await userEndpoint.AddUserToRole(SelectedUser.Id, SelectedAvailableRole);
+
+		UserRoles.Add(SelectedAvailableRole);
+		AvailableRoles.Remove(SelectedAvailableRole);
+	}
+
+	public async void RemoveSelectedRole()
+	{
+		await userEndpoint.RemoveUserFromRole(SelectedUser.Id, SelectedUserRole);
+
+		AvailableRoles.Add(SelectedUserRole);
+		UserRoles.Remove(SelectedUserRole);
 	}
 }
